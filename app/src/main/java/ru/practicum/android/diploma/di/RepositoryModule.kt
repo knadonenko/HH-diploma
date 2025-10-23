c44033b8fcd74234fe42dd8f9f79ca37117c324c
twistofthemyth <twist.of.the.myth@gmail.com>
2025-10-23 21:57:20 +0300

    - обновил версию koin до 4.1.1 - добавил DataModule, RepositoryModule, ViewModelModule

---
+++ app/src/main/java/ru/practicum/android/diploma/di/RepositoryModule.kt
@@ -0,0 +1,7 @@
+package ru.practicum.android.diploma.di
+
+import org.koin.dsl.module
+
+val repositoryModule = module {
+    // Описываем компоненты слоя domain тут (скорее всего это будут только репозитории)
+}

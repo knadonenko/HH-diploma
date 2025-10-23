da85ae06f52b0ffba7f6d9118a11e74af210a2ae
twistofthemyth <twist.of.the.myth@gmail.com>
2025-10-23 22:55:00 +0300

    поправил форматирование

--- app/src/main/java/ru/practicum/android/diploma/domain/api/HelloWorldRepository.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/api/HelloWorldRepository.kt
@@ -2,5 +2,5 @@ package ru.practicum.android.diploma.domain.api
 
 // todo класс для демонстрации работы koin. Удалить перед ПР в main
 interface HelloWorldRepository {
-    fun getGreetings() : String
+    fun getGreetings(): String
 }
--- app/src/main/java/ru/practicum/android/diploma/presentation/HelloWorldViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/HelloWorldViewModel.kt
@@ -6,7 +6,7 @@ import ru.practicum.android.diploma.domain.api.HelloWorldRepository
 // todo класс для демонстрации работы koin. Удалить перед ПР в main
 class HelloWorldViewModel(private val repository: HelloWorldRepository) : ViewModel() {
 
-    fun getGreetings() : String {
+    fun getGreetings(): String {
         return repository.getGreetings()
     }
 }

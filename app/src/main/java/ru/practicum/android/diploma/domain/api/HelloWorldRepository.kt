ce29af4c5cea4eed0d7b2ad9a2fdc9eb4bf57b52
twistofthemyth <twist.of.the.myth@gmail.com>
2025-10-23 22:51:08 +0300

    смелая попытка обойти линтер 2

--- app/src/main/java/ru/practicum/android/diploma/domain/api/HelloWorldRepository.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/api/HelloWorldRepository.kt
@@ -1,6 +1,6 @@
 package ru.practicum.android.diploma.domain.api
 
-// todo 23.10.2025 класс для демонстрации работы koin. Удалить перед ПР в main
+// todo класс для демонстрации работы koin. Удалить перед ПР в main
 interface HelloWorldRepository {
     fun getGreetings() : String
 }

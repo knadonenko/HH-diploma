667814a623ff342ffe1a34155f57c394bbfb15a9
knadonenko <kos891@gmail.com>
2025-10-25 20:41:08 +0300

    правки для детекта

--- app/src/main/java/ru/practicum/android/diploma/ui/theme/Color.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/theme/Color.kt
@@ -2,10 +2,18 @@ package ru.practicum.android.diploma.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val blackUniversal = Color(0xFF1A1B22)
-val white = Color(0xFFFDFDFD)
-val red = Color(0xFF3772E7)
-val blue = Color(0xFF3772E7)
-val grey200 = Color(0xFFE6E8EB)
-val grey500 = Color(0xFFAEAFB4)
-val grey800 = Color(0x1A1B2280)
+const val blackHex = 0xFF1A1B22
+const val whiteHex = 0xFFFDFDFD
+const val redHex = 0xFF3772E7
+const val blueHex = 0xFF3772E7
+const val grey200Hex = 0xFFE6E8EB
+const val grey500Hex = 0xFFE6E8EB
+const val grey800Hex = 0xFFE6E8EB
+
+val blackUniversal = Color(blackHex)
+val white = Color(whiteHex)
+val red = Color(redHex)
+val blue = Color(blueHex)
+val grey200 = Color(grey200Hex)
+val grey500 = Color(grey500Hex)
+val grey800 = Color(grey800Hex)

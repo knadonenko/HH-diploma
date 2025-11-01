a784674d4ed038807edc7cfc4b81a7833cbf055e
twistofthemyth <twist.of.the.myth@gmail.com>
2025-11-02 00:49:19 +0300

    добавил VacancyScreen, VacancyDetails

--- app/src/main/java/ru/practicum/android/diploma/ui/components/VacancyItem.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/components/VacancyItem.kt
@@ -1,30 +1,17 @@
 package ru.practicum.android.diploma.ui.components
 
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
-import coil.request.ImageRequest
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.domain.vacanceis.models.VacanciesInfo
 import ru.practicum.android.diploma.ui.theme.Typography
-import ru.practicum.android.diploma.ui.theme.iconRounding
 import ru.practicum.android.diploma.ui.theme.paddingHalfBase
 
 @Composable
@@ -35,29 +22,7 @@ fun VacancyItem(vacancy: VacanciesInfo, onClick: (VacanciesInfo) -> Unit = {}) {
             .padding(vertical = paddingHalfBase)
             .clickable(onClick = { onClick.invoke(vacancy) })
     ) {
-        Column {
-            val iconModifier = Modifier
-                .size(48.dp)
-                .aspectRatio(1f)
-                .clip(RoundedCornerShape(iconRounding))
-                .border(
-                    width = 1.dp,
-                    color = colorResource(R.color.stroke),
-                    shape = RoundedCornerShape(iconRounding)
-                )
-            AsyncImage(
-                modifier = iconModifier,
-                model = ImageRequest.Builder(LocalContext.current)
-                    .data(vacancy.employerLogo)
-                    .addHeader("User-Agent", "Chrome/138.0.0.0")
-                    .crossfade(true)
-                    .build(),
-                contentDescription = null,
-                contentScale = ContentScale.Crop,
-                placeholder = painterResource(R.drawable.ic_vacancy_placeholder),
-                error = painterResource(R.drawable.ic_vacancy_placeholder),
-            )
-        }
+        VacancyLogo(logo = vacancy.employerLogo)
         Column(
             modifier = Modifier
                 .padding(start = paddingHalfBase)
@@ -72,30 +37,7 @@ fun VacancyItem(vacancy: VacanciesInfo, onClick: (VacanciesInfo) -> Unit = {}) {
                 style = Typography.body16Regular,
                 color = colorResource(R.color.text)
             )
-            val salary = getSalaryText(vacancy = vacancy)
-            Text(
-                text = salary,
-                style = Typography.body16Regular,
-                color = colorResource(R.color.text)
-            )
+            SalaryText(vacancy.salaryFrom, vacancy.salaryTo, vacancy.salaryCurrencySymbol, Typography.body16Regular)
         }
-    }
-}
-
-@Composable
-private fun getSalaryText(vacancy: VacanciesInfo): String {
-    val fromText = stringResource(R.string.from)
-    val toText = stringResource(R.string.to)
-    val noSalaryText = stringResource(R.string.no_salary)
-
-    return when {
-        vacancy.salaryFrom != null && vacancy.salaryTo != null ->
-            "$fromText ${vacancy.salaryFrom} $toText ${vacancy.salaryTo} ${vacancy.salaryCurrencySymbol}"
-
-        vacancy.salaryFrom != null ->
-            "$fromText ${vacancy.salaryFrom} ${vacancy.salaryCurrencySymbol}"
-        vacancy.salaryTo != null ->
-            "$toText ${vacancy.salaryTo} ${vacancy.salaryCurrencySymbol}"
-        else -> noSalaryText
     }
 }
--- app/src/main/java/ru/practicum/android/diploma/ui/screen/VacancyScreen.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/screen/VacancyScreen.kt
@@ -1,16 +1,32 @@
 package ru.practicum.android.diploma.ui.screen
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.domain.vacanceis.models.VacancyDetails
+import ru.practicum.android.diploma.ui.components.SalaryText
+import ru.practicum.android.diploma.ui.components.VacancyLogo
 import ru.practicum.android.diploma.ui.components.topbars.FilterTopBar
+import ru.practicum.android.diploma.ui.theme.Typography
 import ru.practicum.android.diploma.ui.theme.paddingBase
+import ru.practicum.android.diploma.ui.theme.paddingDouble
+import ru.practicum.android.diploma.ui.theme.paddingHalfBase
+import ru.practicum.android.diploma.ui.theme.searchFieldCorner
 
 @Composable
 fun VacancyScreen(modifier: Modifier, onBackClick: () -> Unit) {
@@ -30,7 +46,200 @@ fun VacancyScreen(modifier: Modifier, onBackClick: () -> Unit) {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            /* body */
         }
     }
+}
+
+@Composable
+private fun VacancyBody(vacancy: VacancyDetails) {
+    Column(modifier = Modifier.padding(all = paddingBase)) {
+        VacancyHeader(vacancy)
+        Spacer(modifier = Modifier.padding(top = paddingBase))
+        EmployerDescription(vacancy)
+        Spacer(modifier = Modifier.padding(top = paddingBase))
+        RequiredExperience(vacancy)
+        Spacer(modifier = Modifier.padding(top = paddingDouble))
+        VacancyDescription(vacancy)
+        Spacer(modifier = Modifier.padding(top = paddingDouble))
+        VacancySkills(vacancy)
+        Spacer(modifier = Modifier.padding(top = paddingDouble))
+        EmployerContacts(vacancy)
+    }
+}
+
+@Composable
+private fun VacancyHeader(vacancy: VacancyDetails) {
+    Text(
+        text = vacancy.vacancyName,
+        style = Typography.h1,
+        color = colorResource(R.color.text)
+    )
+    SalaryText(
+        from = vacancy.salaryFrom,
+        to = vacancy.salaryTo,
+        symbol = vacancy.salaryCurrencySymbol,
+        style = Typography.body22Medium
+    )
+}
+
+@Composable
+private fun EmployerDescription(vacancy: VacancyDetails) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(
+                color = colorResource(R.color.stroke),
+                shape = RoundedCornerShape(searchFieldCorner)
+            )
+    ) {
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .padding(all = paddingBase)) {
+            VacancyLogo()
+            Column(
+                modifier = Modifier.padding(start = paddingHalfBase)
+            ) {
+                Text(
+                    text = vacancy.employerName,
+                    style = Typography.body22Medium,
+                    color = colorResource(R.color.text)
+                )
+                Text(
+                    text = vacancy.address ?: vacancy.areaName,
+                    style = Typography.body16Regular,
+                    color = colorResource(R.color.text)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun RequiredExperience(vacancy: VacancyDetails) {
+    Text(
+        text = stringResource(R.string.exp_title),
+        style = Typography.body16Medium,
+        color = colorResource(R.color.text)
+    )
+    Text(
+        text = vacancy.experience ?: stringResource(R.string.no_exp),
+        style = Typography.body16Regular,
+        color = colorResource(R.color.text)
+    )
+    Spacer(modifier = Modifier.padding(top = paddingHalfBase))
+    Text(
+        text = vacancy.schedule ?: "Тип занятости не указан",
+        style = Typography.body16Regular,
+        color = colorResource(R.color.text)
+    )
+}
+
+@Composable
+private fun VacancyDescription(vacancyDetails: VacancyDetails) {
+    Text(
+        text = "Описание вакансии",
+        style = Typography.body22Medium,
+        color = colorResource(R.color.text)
+    )
+    Spacer(modifier = Modifier.padding(top = paddingBase))
+    Text(
+        text = vacancyDetails.description,
+        style = Typography.body16Regular,
+        color = colorResource(R.color.text)
+    )
+}
+
+@Composable
+private fun VacancySkills(vacancy: VacancyDetails) {
+    Text(
+        text = "Ключевые навыки",
+        style = Typography.body22Medium,
+        color = colorResource(R.color.text)
+    )
+    Spacer(modifier = Modifier.padding(top = paddingBase))
+    Text(
+        text = vacancy.skills.joinToString("\n"),
+        style = Typography.body16Regular,
+        color = colorResource(R.color.text)
+    )
+}
+
+@Composable
+private fun EmployerContacts(vacancy: VacancyDetails) {
+    if (vacancy.employerEmail != null && vacancy.employerPhone.isNotEmpty()) {
+        Text(
+            text = "Контакты",
+            style = Typography.body22Medium,
+            color = colorResource(R.color.text)
+        )
+        Spacer(modifier = Modifier.padding(top = paddingBase))
+        if (vacancy.employerPhone.isNotEmpty()) {
+            Text(
+                text = "Номер телефона",
+                style = Typography.body16Medium,
+                color = colorResource(R.color.text)
+            )
+            vacancy.employerPhone.forEach { it -> EmployerPhone(it) }
+            Spacer(modifier = Modifier.padding(top = paddingBase))
+        }
+        if (vacancy.employerEmail != null) {
+            Text(
+                text = "Электронная почта",
+                style = Typography.body16Medium,
+                color = colorResource(R.color.text)
+            )
+            Text(
+                text = vacancy.employerEmail,
+                style = Typography.body16Regular,
+                color = colorResource(R.color.text)
+            )
+        }
+    }
+}
+
+@Composable
+private fun EmployerPhone(phone: VacancyDetails.EmployerPhone) {
+    Row {
+        Text(
+            text = phone.phone,
+            style = Typography.body16Regular,
+            color = colorResource(R.color.text)
+        )
+        if (phone.comment != null) {
+            Text(
+                modifier = Modifier.padding(start = paddingBase),
+                text = phone.comment,
+                style = Typography.body16Regular,
+                color = colorResource(R.color.text)
+            )
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+@Suppress("MagicNumber")
+private fun VacancyBodyPreview() {
+    val vacancyDetails = VacancyDetails(
+        id = "mock",
+        vacancyName = "Переворчиватель пингвинов",
+        address = "Большая Грузинская, д.1с99, Москва",
+        salaryFrom = 1000,
+        salaryTo = 100000,
+        salaryCurrencySymbol = "$",
+        employerName = "Московский зоопарк",
+        employerPhone = listOf(
+            VacancyDetails.EmployerPhone("+7 (495) 775-33-70", "основной"),
+            VacancyDetails.EmployerPhone("+7 (495) 775-33-70")
+        ),
+        employerEmail = "zoopark@culture.mos.ru",
+        schedule = "Полный день",
+        areaName = "Москва",
+        experience = "Не требуется",
+        description = "Ответственность за переворачивание пингвинов, упавших на спину.\n" +
+            "Контроль за правильным положением птиц. Взаимодействие с ветеринарной службой.\n" +
+            "Наблюдение за поведением пингвинов в вольере.",
+        skills = listOf("Работа с животными", "Внимательность", "Физическая выносливость", "Любовь к птицам")
+    )
+    VacancyBody(vacancyDetails)
 }

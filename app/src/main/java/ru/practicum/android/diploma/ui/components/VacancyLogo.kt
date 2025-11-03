d65d30c544b72e39c12558b92a270d86169aa125
knadonenko <kos891@gmail.com>
2025-11-03 20:54:22 +0300

    фиксы размеров

--- app/src/main/java/ru/practicum/android/diploma/ui/components/VacancyLogo.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/components/VacancyLogo.kt
@@ -12,7 +12,6 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import ru.practicum.android.diploma.R
